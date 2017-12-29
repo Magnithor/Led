1645a794037df1df6a0f8e7df14d464469930063
Magni <magni.thor@gmail.com>
2017-12-29 23:04:06 +0000

    config

--- main.cpp
+++ main.cpp
@@ -1,21 +1,24 @@
 #include <signal.h>
+#include <sstream>
 #include "fastgpioomega2.h"
 #include "apa102.h"
 #include "httpServer.h"
 #include "urls.h"
 #include "playBack.h"
 #include "playBackItemSolid.h"
+#include "json.h"
 
-
-void httpResponse(HttpConnection* httpConnection);
-
-volatile  int lastSignal = -1;
-HttpServer httpServer(8080, &httpResponse);
 #define ledcount  144*3
 APA102 apa102((uint8_t)0, (uint8_t)1, (uint16_t) ledcount);
 PlayBack playBack(&apa102);
 
 Urls urls(&apa102, &playBack);
+
+
+HttpServer* httpServer;
+void httpResponse(HttpConnection* httpConnection);
+
+volatile  int lastSignal = -1;
 
 void httpResponse(HttpConnection* httpConnection) {
     urls.httpResponse(httpConnection);
@@ -37,19 +40,20 @@ void check() {
 		lastSignal = -1;
 		printf( "\nShuting down...\n");
 		printf( " * Close http\n");
-		httpServer.closeAll();
+		httpServer->closeAll();               
 		printf( " * turn off led\n");				
 		playBack.turnOff();
 		printf( "exit\n");
+                delete httpServer;
 		exit(0);
 	}
 
-	httpServer.check();
+	httpServer->check();
 	playBack.update();
 }
 
 void startService(){
-	        /* Our process ID and Session ID */
+	/* Our process ID and Session ID */
         pid_t pid, sid;
 
         /* Fork off the parent process */
@@ -90,8 +94,46 @@ void startService(){
 
 }
 
+void loadConfig(json::Object &config){
+        std::ifstream inFile;
+        inFile.open("led.json");//open the input file
+
+        std::stringstream strStream;
+        strStream << inFile.rdbuf();//read the file
+        config.parse(strStream.str());
+}
+
+std::string httpConfigKey = std::string("httpServer");
+std::string httpConfigPort = std::string("port");
+
 int main(int argc, char* argv[])
 {	
+        json::Object config;
+        loadConfig(config);
+        if (!config.hasKey(httpConfigKey)){
+                printf("config missing %s\n", httpConfigKey.c_str());
+                return -1;
+        }
+
+        if (!config.get(httpConfigKey)->isObject()){
+                printf("config %s is not object\n",httpConfigKey.c_str());
+                return -1;
+        }
+
+        json::Object *httpConfig = config.get(httpConfigKey)->getObject();
+        if (!httpConfig->hasKey(httpConfigPort)){
+                printf("config missing %s in %s\n", httpConfigPort.c_str(), httpConfigKey.c_str());
+                return -1;
+        }
+        if (!httpConfig->get(httpConfigPort)->isInt()){
+                printf("config %s in %s are not int\n", httpConfigPort.c_str(), httpConfigKey.c_str());
+                return -1;                
+        }
+        int httpPort = httpConfig->get(httpConfigPort)->getInt();
+
+        httpServer = new HttpServer(httpPort, &httpResponse);
+
+
 	PlayBackItemSolid *solid = new PlayBackItemSolid(&apa102, (uint8_t)0,(uint8_t)1,(uint8_t)0,(uint8_t)1);
 	playBack.push(solid);
 
@@ -102,7 +144,7 @@ int main(int argc, char* argv[])
 		  return -1;
 	}
 	
-	httpServer.start();
+	httpServer->start();
 
 	int wait = 500;
 
